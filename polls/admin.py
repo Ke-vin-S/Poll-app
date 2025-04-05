562bc31deb652afedd7945b2f7b0dcfb025e1cc1
Kevin Sanjula <kevinxsanjula@gmail.com>
2025-04-05 16:47:34 +0000

    Part 7: admin tabular choices, List display ,Search

--- polls/admin.py
+++ polls/admin.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
-
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -14,5 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"]}),
     ]    
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 admin.site.register(Question, QuestionAdmin)
